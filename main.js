76d6fc09cec44191c8e76c94f46159ff0267e8a7
ddor712 <dannydor712@gmail.com>
2023-07-17 12:14:17 -0500

    The html file has been added and I have included the code to open a Render Window for it to show.

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("main function works");
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width:800,
+        height:600
+    })
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    }) // This is for mac if the app is running and no windows are open it will open one
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
